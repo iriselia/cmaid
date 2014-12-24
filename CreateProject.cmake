5afaaf70a59bff67eadb9cc1f1a80e1ac4dd3cfd
Frank Park <ypiao@wpi.edu>
2014-12-24 10:04:24 -0500

    Initial Commit

--- CreateProject.cmake
+++ CreateProject.cmake
@@ -1,5 +1,9 @@
 cmake_minimum_required( VERSION 2.8 )
 
+MACRO(recursive_include out_dir projName)
+	list(APPEND out_dir ${${currentName}_PUBLIC_INCLUDE_DIRS})
+ENDMACRO()
+
 #
 #
 #
@@ -9,13 +13,22 @@ MACRO(create_project mode in_macros includes linkLibs)
 	#----- Create Project -----
 	get_folder_name(${CMAKE_CURRENT_SOURCE_DIR} PROJECT_NAME)
 	project( ${PROJECT_NAME} )
-
+	
+	#----- Unset Cached Arguments -----
+	unset(${PROJECT_NAME}_INCLUDE_PROJECTS CACHE)
+	unset(${PROJECT_NAME}_MODE CACHE)
+	set(${PROJECT_NAME}_MODE "${mode}" CACHE STRING "")
 	#----- SCAN SOURCE -----
-	file(GLOB_RECURSE MY_SRC ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.c)
-	file(GLOB_RECURSE MY_CPP_SRC ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp)
-	if( NOT MY_SRC STREQUAL "" )
-		create_source_group("" "${CMAKE_CURRENT_SOURCE_DIR}/" ${MY_SRC})
+	if( NOT ${PROJECT_NAME}_SRC )
+		file(GLOB_RECURSE ${PROJECT_NAME}_SRC ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp ${CMAKE_CURRENT_SOURCE_DIR}/*.c)
+		set( ${PROJECT_NAME}_SRC "${${PROJECT_NAME}_SRC}" CACHE STRING "" )
+		if( ${PROJECT_NAME}_SRC )
+			create_source_group("" "${CMAKE_CURRENT_SOURCE_DIR}/" ${${PROJECT_NAME}_SRC})
+		endif()
+		set(build_flag OFF)
 	endif()
+	
+	file(GLOB_RECURSE MY_CPP_SRC ${CMAKE_CURRENT_SOURCE_DIR}/*.cxx ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp)
 
 	file(GLOB_RECURSE MY_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/*.h ${CMAKE_CURRENT_SOURCE_DIR}/*.hpp ${CMAKE_CURRENT_SOURCE_DIR}/*.inl ${CMAKE_CURRENT_SOURCE_DIR}/*.resx)
 	if( NOT MY_HEADERS STREQUAL "" )
@@ -32,7 +45,7 @@ MACRO(create_project mode in_macros includes linkLibs)
 		create_source_group("" "${CMAKE_CURRENT_SOURCE_DIR}/" ${MY_MISC})
 	endif()
 
-	if( (MY_SRC STREQUAL "") AND (MY_HEADERS STREQUAL "") )
+	if( (${PROJECT_NAME}_SRC STREQUAL "") AND (MY_HEADERS STREQUAL "") )
 		message(STATUS "Project is empty, a stub C header was created to set compiler language.")
 		file(WRITE Stub.h "")
 		LIST(APPEND MY_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/Stub.h)
@@ -59,7 +72,7 @@ MACRO(create_project mode in_macros includes linkLibs)
 
 	# Force C++ if there's any cpp file
 	if(MY_CPP_SRC)
-		set_source_files_properties(${MY_SRC} PROPERTIES LANGUAGE CXX)
+		set_source_files_properties(${${PROJECT_NAME}_SRC} PROPERTIES LANGUAGE CXX)
 	endif()
 
 	#----- Mark PRECOMPILED HEADER -----
@@ -68,9 +81,9 @@ MACRO(create_project mode in_macros includes linkLibs)
 			GET_FILENAME_COMPONENT(PRECOMPILED_HEADER_NAME ${PRECOMPILED_HEADER} NAME)
 			GET_FILENAME_COMPONENT(PRECOMPILED_SOURCE_NAME ${PRECOMPILED_SOURCE} NAME)			
 			GET_FILENAME_COMPONENT(PRECOMPILED_BASENAME ${PRECOMPILED_HEADER_NAME} NAME_WE)
-			SET(PRECOMPILED_BINARY "${PRECOMPILED_BASENAME}\$(Configuration).pch")
+			SET(PRECOMPILED_BINARY "${PRECOMPILED_BASENAME}-$(Configuration).pch")
 			
-			SET_SOURCE_FILES_PROPERTIES(${MY_SRC}
+			SET_SOURCE_FILES_PROPERTIES(${${PROJECT_NAME}_SRC}
 									PROPERTIES COMPILE_FLAGS "/Yu\"${PRECOMPILED_HEADER_NAME}\" /FI\"${PRECOMPILED_HEADER_NAME}\" /Fp\"${PRECOMPILED_BINARY}\""
 											   OBJECT_DEPENDS "${PRECOMPILED_BINARY}")  
 											   
@@ -80,43 +93,177 @@ MACRO(create_project mode in_macros includes linkLibs)
 		ENDIF(MSVC)
 	endif()
 
+	if(NOT build_flag EQUAL OFF)
+		#----- Add Preprocessor Definitions -----
+		foreach(currMacro ${${PROJECT_NAME}_macros})
+			add_definitions("-D${currMacro}")
+		endforeach()
+		#----- Add Project Name -----
+		add_definitions("-DPROJECT_NAME=\"${PROJECT_NAME}\"")
+		add_definitions("-DPROJECT_ID=${PROJECT_COUNT}")
+		
+		#------ INCLUDE DIRS AND LIBS -----
+		include( ProjectSettingsTemplate )
+		# Must include self
+		include_directories( ${${PROJECT_NAME}_ALL_INCLUDE_DIRS} )
+		# Process include list, an element could be a list of dirs or a target name
+		set(includeDirs "")
+		set(includeProjs "")
+		FOREACH(currentName ${includes})
+			if(EXISTS ${currentName})
+				# if exists, it is a directory
+				list(APPEND includeDirs ${currentName})
+			else()
+				# if doesn't exist, it is a target, we retrieve the include dirs by appending _INCLUDE_DIRS to its name
+				list(APPEND includeDirs ${${currentName}_PUBLIC_INCLUDE_DIRS})
+				list(APPEND includeDirs ${${currentName}_PROTECTED_INCLUDE_DIRS})
+				list(APPEND includeDirs ${${currentName}_SOURCE_DIR})
+				list(APPEND includeProjs ${currentName})
+			endif()
+		ENDFOREACH(currentName ${includes})
+		set(${PROJECT_NAME}_INCLUDE_PROJECTS "${includeProjs}" CACHE STRING "")
+		include_directories(${includeDirs})
+		# Add links
+		link_libraries(${linkLibs})
+				
+		#----- CREATE TARGET -----
+		if(${${PROJECT_NAME}_MODE} STREQUAL "STATIC")
+			add_library (${PROJECT_NAME} STATIC ${${PROJECT_NAME}_SRC} ${MY_HEADERS} ${MY_MISC} ${MY_RESOURCES})
+			add_definitions("-DIS_STATIC")
+			add_definitions("-DSTATIC_ID=${PROJECT_COUNT}")
+		elseif(${${PROJECT_NAME}_MODE} STREQUAL "DYNAMIC" OR ${${PROJECT_NAME}_MODE} STREQUAL "SHARED" )
+			add_library (${PROJECT_NAME} SHARED ${${PROJECT_NAME}_SRC} ${MY_HEADERS} ${MY_MISC} ${MY_RESOURCES})
+			add_definitions("-DIS_DYNAMIC")
+			add_definitions("-DEXPORT_ID=${PROJECT_COUNT}")
+		elseif(${${PROJECT_NAME}_MODE} STREQUAL "CONSOLE")
+			add_executable (${PROJECT_NAME} ${${PROJECT_NAME}_SRC} ${MY_HEADERS} ${MY_SHADERS} ${MY_RESOURCES} ${MY_MISC})
+		elseif(${${PROJECT_NAME}_MODE} STREQUAL "WIN32")
+			add_executable (${PROJECT_NAME} WIN32 ${${PROJECT_NAME}_SRC} ${MY_HEADERS} ${MY_SHADERS} ${MY_RESOURCES} ${MY_MISC})
+		endif()
+		
+		#------ force include PCH -----
+		if( NOT MY_PRECOMPILED_HEADER STREQUAL "" AND NOT MY_PRECOMPILED_SOURCE STREQUAL "")
+			###DON'T ENABLE get_target_property(FLAGS ${PROJECT_NAME} COMPILE_FLAGS)
+			set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "${FLAGS} /FI\"${MY_PRECOMPILED_HEADER}\"")
+		endif()
+		
+		#------ set target filter -----
+		if( MSVC )
+			# TODO: OPTIMIZE THIS
+			string(REPLACE "/" ";" sourceDirList "${CMAKE_SOURCE_DIR}")
+			string(REPLACE "/" ";" currSourceDirList "${CMAKE_CURRENT_SOURCE_DIR}")
+			list(REVERSE currSourceDirList)
+			list(REMOVE_AT currSourceDirList 0)
+			list(REVERSE currSourceDirList)
+			foreach(sourceDir ${sourceDirList})
+				list(REMOVE_AT currSourceDirList 0)
+			endforeach()
+			list(LENGTH currSourceDirList listLength)
+			string(REPLACE ";" "/" filterDir "${currSourceDirList}")
+		
+			SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON)
+			SET_PROPERTY(TARGET ${PROJECT_NAME}		PROPERTY FOLDER ${filterDir})
+		endif()
+		
+		#------ need linker language flag for header only static libraries -----
+		SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES LINKER_LANGUAGE CXX)
+		
+		#----- Custom PreBuild Target ------
+		# Flex and Bison
+		if( USE_FLEX_AND_BISON )
+			include( Optional/AddFlexBisonCustomTarget )
+		endif()
+		# Shader Copy
+		if( NOT MY_SHADERS STREQUAL "" )
+			add_custom_target(${PROJECT_NAME}PreBuild ALL
+				COMMAND ${CMAKE_COMMAND}
+				-DSrcDir=${CMAKE_CURRENT_SOURCE_DIR}
+				-DDestDir=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}
+				-P ${CMAKE_MODULE_PATH}/CopyResource.cmake
+				COMMENT "Copying resource files to the binary output directory")
+				
+			add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}PreBuild)
+				
+			if( MSVC )
+				SET_PROPERTY(GLOBAL PROPERTY USE_FOLDERS ON)
+				SET_PROPERTY(TARGET ${PROJECT_NAME}PreBuild		PROPERTY FOLDER CMakePredefinedTargets)
+			endif()
+		endif()
+	endif()
+ENDMACRO(create_project mode linLibraries)
+
+MACRO(create_project_helper target_folder)
+	#----- Create Project -----
+	get_folder_name(${target_folder} PROJECT_NAME)
+	project( ${PROJECT_NAME} )
+
+	FILE(RELATIVE_PATH rel_dir ${PROJECT_SOURCE_DIR} ${target_folder})
+	
+
+	message("${PROJECT_NAME} ${target_folder}")
+	set(PROJECT_BINARY_DIR "${PROJECT_BINARY_DIR}/${rel_dir}")
+	set(${PROJECT_NAME}_BINARY_DIR "${${PROJECT_NAME}_BINARY_DIR}/${rel_dir}")
+	#----- Order is important!!! -----
+	set(PROJECT_SOURCE_DIR ${target_folder})
+	set(${PROJECT_NAME}_SOURCE_DIR ${target_folder})
+	message("
+	PROJECT_SOURCE_DIR ${PROJECT_SOURCE_DIR}
+	PROJECT-NAME_SOURCE_DIR ${${PROJECT_NAME}_SOURCE_DIR}
+	PROJECT_BINARY_DIR ${PROJECT_BINARY_DIR}
+	PROJECT-NAME_BINARY_DIR ${${PROJECT_NAME}_BINARY_DIR}
+	")
+
 	#----- Add Preprocessor Definitions -----
-	foreach(currMacro ${in_macros})
+	foreach(currMacro ${${PROJECT_NAME}_macros})
 		add_definitions("-D${currMacro}")
 	endforeach()
+	#----- Add Project Name -----
+	add_definitions("-DPROJECT_NAME=\"${PROJECT_NAME}\"")
+	add_definitions("-DPROJECT_ID=${PROJECT_COUNT}")
 	
 	#------ INCLUDE DIRS AND LIBS -----
 	include( ProjectSettingsTemplate )
 	# Must include self
-	include_directories( ${${PROJECT_NAME}_INCLUDE_DIRS} )
+	include_directories( ${${PROJECT_NAME}_ALL_INCLUDE_DIRS} )
 	# Process include list, an element could be a list of dirs or a target name
+	set(includeDirs "")
+	set(includeProjs "")
 	FOREACH(currentName ${includes})
 		if(EXISTS ${currentName})
 			# if exists, it is a directory
 			list(APPEND includeDirs ${currentName})
 		else()
 			# if doesn't exist, it is a target, we retrieve the include dirs by appending _INCLUDE_DIRS to its name
-			list(APPEND includeDirs ${${currentName}_INCLUDE_DIRS})
+			list(APPEND includeDirs ${${currentName}_PUBLIC_INCLUDE_DIRS})
+			list(APPEND includeProjs ${currentName})
 		endif()
 	ENDFOREACH(currentName ${includes})
+	set(${PROJECT_NAME}_INCLUDE_PROJECTS "${includeProjs}" CACHE STRING "")
 	include_directories(${includeDirs})
 	# Add links
 	link_libraries(${linkLibs})
 	
+#	message("HELPER: ${PROJECT_NAME} src: ${${PROJECT_NAME}_SRC}")
+	
 	#----- CREATE TARGET -----
-	if(${mode} STREQUAL "STATIC")
-		add_library (${PROJECT_NAME} STATIC ${MY_SRC} ${MY_HEADERS} ${MY_MISC} ${MY_RESOURCES})
-	elseif(${mode} STREQUAL "DYNAMIC" OR ${mode} STREQUAL "SHARED" )
+#	message("MODE: ${${PROJECT_NAME}_MODE}")
+	if(${${PROJECT_NAME}_MODE} STREQUAL "STATIC")
+		add_library (${PROJECT_NAME} STATIC ${${PROJECT_NAME}_SRC} ${MY_HEADERS} ${MY_MISC} ${MY_RESOURCES})
+		add_definitions("-DIS_STATIC")
+		add_definitions("-DSTATIC_ID=${PROJECT_COUNT}")
+	elseif(${${PROJECT_NAME}_MODE} STREQUAL "DYNAMIC" OR ${${PROJECT_NAME}_MODE} STREQUAL "SHARED" )
 		add_library (${PROJECT_NAME} SHARED ${MY_SRC} ${MY_HEADERS} ${MY_MISC} ${MY_RESOURCES})
-	elseif(${mode} STREQUAL "CONSOLE")
+		add_definitions("-DIS_DYNAMIC")
+		add_definitions("-DEXPORT_ID=${PROJECT_COUNT}")
+	elseif(${${PROJECT_NAME}_MODE} STREQUAL "CONSOLE")
 		add_executable (${PROJECT_NAME} ${MY_SRC} ${MY_HEADERS} ${MY_SHADERS} ${MY_RESOURCES} ${MY_MISC})
-	elseif(${mode} STREQUAL "WIN32")
+	elseif(${${PROJECT_NAME}_MODE} STREQUAL "WIN32")
 		add_executable (${PROJECT_NAME} WIN32 ${MY_SRC} ${MY_HEADERS} ${MY_SHADERS} ${MY_RESOURCES} ${MY_MISC})
 	endif()
 	
 	#------ force include PCH -----
 	if( NOT MY_PRECOMPILED_HEADER STREQUAL "" AND NOT MY_PRECOMPILED_SOURCE STREQUAL "")
-		#get_target_property(FLAGS ${PROJECT_NAME} COMPILE_FLAGS)
+		get_target_property(FLAGS ${PROJECT_NAME} COMPILE_FLAGS)
 		set_target_properties(${PROJECT_NAME} PROPERTIES COMPILE_FLAGS "${FLAGS} /FI\"${MY_PRECOMPILED_HEADER}\"")
 	endif()
 	
@@ -124,7 +271,7 @@ MACRO(create_project mode in_macros includes linkLibs)
 	if( MSVC )
 		# TODO: OPTIMIZE THIS
 		string(REPLACE "/" ";" sourceDirList "${CMAKE_SOURCE_DIR}")
-		string(REPLACE "/" ";" currSourceDirList "${CMAKE_CURRENT_SOURCE_DIR}")
+		string(REPLACE "/" ";" currSourceDirList "${target_folder}")
 		list(REVERSE currSourceDirList)
 		list(REMOVE_AT currSourceDirList 0)
 		list(REVERSE currSourceDirList)
@@ -150,7 +297,7 @@ MACRO(create_project mode in_macros includes linkLibs)
 	if( NOT MY_SHADERS STREQUAL "" )
 		add_custom_target(${PROJECT_NAME}PreBuild ALL
 			COMMAND ${CMAKE_COMMAND}
-			-DSrcDir=${CMAKE_CURRENT_SOURCE_DIR}
+			-DSrcDir=${target_folder}
 			-DDestDir=${CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG}
 			-P ${CMAKE_MODULE_PATH}/CopyResource.cmake
 			COMMENT "Copying resource files to the binary output directory")
@@ -163,4 +310,4 @@ MACRO(create_project mode in_macros includes linkLibs)
 		endif()
 	endif()
 
-ENDMACRO(create_project mode linLibraries)
+ENDMACRO()
