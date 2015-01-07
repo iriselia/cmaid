cd9e58a862cf907e437a683731a8708b199d4ce4
Frank Park <ypiao@wpi.edu>
2015-01-06 19:54:32 -0500

    Add module interface

--- Core/SetOutputDirectories.cmake
+++ Core/SetOutputDirectories.cmake
@@ -1,8 +1,8 @@
-SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/Binaries CACHE PATH "Single Directory for all executables." )
-SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/Binaries CACHE PATH "Single Directory for all executables." )
-SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_SOURCE_DIR}/Binaries CACHE PATH "Single Directory for all executables." )
-SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${CMAKE_SOURCE_DIR}/Binaries CACHE PATH "Single Directory for all executables." )
-SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_SOURCE_DIR}/Binaries CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/Binaries/Backup CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/Binaries/Backup CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_SOURCE_DIR}/Binaries/Backup CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_MINSIZEREL ${CMAKE_SOURCE_DIR}/Binaries/Backup CACHE PATH "Single Directory for all executables." )
+SET( CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELWITHDEBINFO ${CMAKE_SOURCE_DIR}/Binaries/Backup CACHE PATH "Single Directory for all executables." )
 
 SET( CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/Binaries/Libraries CACHE PATH "Single Directory for all static libraries." )
 SET( CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/Binaries/Libraries/Release CACHE PATH "Single Directory for all static libraries." )
@@ -17,11 +17,11 @@ SET( CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${CMAKE_SOURCE_DIR}/Binaries/Libraries
 SET( CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${CMAKE_SOURCE_DIR}/Binaries/Libraries/RelWithDebInfo CACHE PATH "Single Directory for all static libraries.")
 
 SET( CMAKE_PDB_OUTPUT_DIRECTORY ${CMAKE_SOURCE_DIR}/Binaries/Libraries CACHE PATH "Single Directory for all static libraries." )
-SET( CMAKE_DEBUG_POSTFIX "-d" )
+SET( CMAKE_DEBUG_POSTFIX "-debug" )
 SET( CMAKE_INCLUDE_CURRENT_DIR ON )
 
-file(MAKE_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
-file(MAKE_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+#file(MAKE_DIRECTORY ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+#file(MAKE_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
 
 if(WIN32)
 add_definitions( "-DPLATFORM_WINDOWS" )
