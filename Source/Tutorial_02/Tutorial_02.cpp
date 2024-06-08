9833ad70b2274ec2a5a2037a651f0cc0ee6db807
Iriselia <5825014+iriselia@users.noreply.github.com>
2024-06-07 20:53:59 -0700

    upload tutorial 02

---
+++ Source/Tutorial_02/Tutorial_02.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include "Tutorial_02_Static.h"
+#include "Tutorial_02_Shared.h"
+
+// Tutorial 2: How to use static and shared libraries.
+int main()
+{
+#ifdef TUTORIAL_02_STATIC
+	Tutorial_02_Static_Struct StaticStruct;
+	StaticStruct.PrintName();
+#endif
+
+#ifdef TUTORIAL_02_SHARED
+	Tutorial_02_Shared_Struct SharedStruct;
+	SharedStruct.PrintName();
+#endif
+	printf("Press any key to exit...\n");
+	getchar();
+	return 0;
+}
