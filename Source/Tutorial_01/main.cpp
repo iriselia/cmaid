102767932d06474a6339a55d610165c0c7711097
Iriselia <5825014+iriselia@users.noreply.github.com>
2024-06-07 20:53:58 -0700

    upload tutorial01

--- Source/Tutorial_01/main.cpp
+++ Source/Tutorial_01/main.cpp
@@ -1,46 +1,14 @@
+#include <cstdio>
 
 // Tutorial 1: EXAMPLE_MACRO is defined in the CMakeLists.txt of the Tutorial_01 project
-#ifdef EXAMPLE_MACRO
 int main()
 {
-	// ExampleObject.h is force included through the .pch.h pre-compiled header
-	auto pLocalObject = new ::LocalObject;
-	pLocalObject->SayHi();
-	delete pLocalObject;
-
-	/*
-	Example 1: Pre-compiled header.
-
-	PublicDLL is included through SampleProgram.pch.h and SampleProgram.pch.cpp.
-	Any *.pch.h and *.pch.cpp source files are treated as pre-compiled and will
-	be force-included by Visual Studio.
-	*/
-	auto pDLLObject = new PublicDLL::DLLObject;
-	/*
-	Example 2 Part 1: Module visibility.
-
-	The implementation of AccessPrivate() involves accessing PrivateLibObject which is
-	invisible to SampleProgram. It is because the CMakeLists.txt in SampleProgram
-	defines SampleProgram only contains	include directories for PublicDLL.
-	*/
-	pDLLObject->AccessPrivate();
-	pDLLObject->SayHi();
-	delete pDLLObject;
-
-	/* 
-	Example 2 Part 2: Module visibility.
-	Activating the code below should cause an error, because PrivateLibObject is
-	invisible in this namespace but available to PublicDLL.
-	See DLLObject() Constructor for details. 
-	*/
-	//* Comment this line to activate the code: "// /*"
-	auto pLibObject = new PrivateLib::PrivateLibObject;
-	pLibObject->SayHi();
-	delete pLibObject;
-	//*/
-
-	printf("\nPress any key to exit...\n");
+#ifdef EXAMPLE_MACRO
+	printf("EXAMPLE_MACRO is defined!\n");
+#else
+	printf("EXAMPLE_MACRO is not defined!\n");
+#endif
+	printf("Press any key to exit...\n");
 	getchar();
 	return 0;
 }
-#endif
